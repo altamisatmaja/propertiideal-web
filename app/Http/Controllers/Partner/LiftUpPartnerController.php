ede2bbf7f173a54fec7e714eca773ff6c5addc5c
altamisatmaja <altamisfattah16@gmail.com>
2024-05-11 00:08:50 +0700

    fix: some file that doesnt saved

--- app/Http/Controllers/Partner/LiftUpPartnerController.php
+++ app/Http/Controllers/Partner/LiftUpPartnerController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Partner;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class LiftUpPartnerLayout extends Controller
+class LiftUpPartnerController extends Controller
 {
     //
 }
